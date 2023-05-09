701c827b1553c92d3c4610dc7f9d24d793bf7769
aakmataliev04 <68242135+aakmataliev04@users.noreply.github.com>
2023-05-10 03:07:42 +0600

    Delete onlineShop directory

--- onlineShop/script.js
+++
@@ -1,8 +0,0 @@
-let hamburger = document.querySelector('.headerHamburger');
-let menu = document.querySelector('.header');
-let zeroScroll = document.querySelector('body');
-
-hamburger.addEventListener('click', function () {
-    menu.classList.toggle('active');
-    zeroScroll.classList.toggle('lock');
-});
